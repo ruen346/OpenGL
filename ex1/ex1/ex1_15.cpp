7edf7ece8fc2da7ca19b5b9ceee04f21b04582ef
Ruen_note <ruen346@gmail.com>
2018-09-27 23:18:35 +0900

    15번 거의 끝남
    와 타노시

--- ex1/ex1/ex1_15.cpp
+++ ex1/ex1/ex1_15.cpp
@@ -6,21 +6,111 @@ using namespace std;
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 
+int times[6];
+
 void one()
 {
+	glPushMatrix();
+	glTranslatef(80, -60, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(80, 20, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
 }
 void two()
 {
+	glPushMatrix();
+	glTranslatef(40, -100, 0);
+	glScalef(3, 1, 1);
+	glutWireCube(20);
+	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(80, -60, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(40, -20, 0);
+	glScalef(3, 1, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0, 20, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(40, 60, 0);
+	glScalef(3, 1, 1);
+	glutWireCube(20);
+	glPopMatrix();
 }
 void three()
 {
+	glPushMatrix();
+	glTranslatef(40, -100, 0);
+	glScalef(3, 1, 1);
+	glutWireCube(20);
+	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(80, -60, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(40, -20, 0);
+	glScalef(3, 1, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(80, 20, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(40, 60, 0);
+	glScalef(3, 1, 1);
+	glutWireCube(20);
+	glPopMatrix();
 }
 void four()
 {
+	glPushMatrix();
+	glTranslatef(0, -60, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(80, -60, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(40, -20, 0);
+	glScalef(3, 1, 1);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(80, 20, 0);
+	glScalef(1, 3, 1);
+	glutWireCube(20);
+	glPopMatrix();
 }
 void five()
 {
@@ -44,11 +134,61 @@ void nine()
 }
 void zero()
 {
+	glPushMatrix();
+	glTranslatef(40, -100, 0);
+	glScalef(3, 1, 1);
+	glutSolidCube(20);
+	glutWireCube(20);
+	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(0, -60, 0);
+	glScalef(1, 3, 1);
+	glutSolidCube(20);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(80, -60, 0);
+	glScalef(1, 3, 1);
+	glutSolidCube(20);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0, 20, 0);
+	glScalef(1, 3, 1);
+	glutSolidCube(20);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(80, 20, 0);
+	glScalef(1, 3, 1);
+	glutSolidCube(20);
+	glutWireCube(20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(40, 60, 0);
+	glScalef(3, 1, 1);
+	glutSolidCube(20);
+	glutWireCube(20);
+	glPopMatrix();
 }
 
 void Timer(int value)
 {
+	time_t now = time(0);
+	struct tm curr_time;
+	localtime_s(&curr_time, &now);
+	times[0] = curr_time.tm_hour / 10;
+	times[1] = curr_time.tm_hour % 10;
+	times[2] = curr_time.tm_min / 10;
+	times[3] = curr_time.tm_min % 10;
+	times[4] = curr_time.tm_sec / 10;
+	times[5] = curr_time.tm_sec % 10;
+	cout << times[0] << times[1] << "½Ã" << times[2] << times[3] << "ºÐ" << times[4] << times[5] << "ÃÊ" << endl;
 
 	glutPostRedisplay();
 	glutTimerFunc(20, Timer, 1);
@@ -107,7 +247,45 @@ GLvoid drawScene(GLvoid)
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 
-	
+	glTranslatef(-300, 0, 0.0);
+
+	for (int i = 0; i < 6; i++)
+	{
+		glTranslatef(100, 0, 0);
+		switch (times[i])
+		{
+		case 0:
+			zero();
+			break;
+		case 1:
+			one();
+			break;
+		case 2:
+			two();
+			break;
+		case 3:
+			three();
+			break;
+		case 4:
+			four();
+			break;
+		case 5:
+			five();
+			break;
+		case 6:
+			six();
+			break;
+		case 7:
+			seven();
+			break;
+		case 8:
+			eight();
+			break;
+		case 9:
+			nine();
+			break;
+		}
+	}
 
 	glPopMatrix();
 
