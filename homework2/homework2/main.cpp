2efbd692ec0c25b0396b4fd38ebb885f665cbbec
ruen_mainpc <ruen346@gmail.com>
2018-11-29 02:44:32 +0900

    롤코 트트랙 끝?

--- homework2/homework2/main.cpp
+++ homework2/homework2/main.cpp
@@ -21,7 +21,7 @@ struct Barrier
 struct Coster
 {
 	float x = 0;
-	float y = 0;
+	float y = -30;//2d 이상해서 그럼 거꾸로생각 ㄱㄱ
 	float z = 0;
 	int active = 0;
 };
@@ -36,8 +36,14 @@ int mod = 2;//1:2d높이 2:2d탑뷰 3:3d
 int mod1_right_coster = 20;//우클릭시 지금 선택한 코스터 눌림 디폴트:20
 int mod2_right_coster = 20;//우클릭시 지금 선택한 코스터 눌림 디폴트:20
 
+int cart_x;
+int cart_y;
+int cart_z;
+int cart_go = 0;//50으로 나눠서 몇번째 점을 지나는지
+
 int x_ro = 0;
 int y_ro = 0;
+int z_zoom = 0;
 
 
 void SetupRC()
@@ -52,7 +58,38 @@ void SetupRC()
 
 void Timer(int value)
 {
-	
+	if (mod == 3)
+	{
+		if (cart_go < coster_count * 50)
+			cart_go++;
+		else
+			cart_go = 0;
+
+		int p[4];
+
+		//카트위치
+		p[0] = cart_go / 50 - 1;
+		p[1] = cart_go / 50;
+		p[2] = cart_go / 50 + 1;
+		p[3] = cart_go / 50 + 2;
+
+		if (p[0] == -1)
+			p[0] = coster_count - 1;
+		if (p[2] >= coster_count)
+			p[2] -= coster_count;
+		if (p[3] >= coster_count)
+			p[3] -= coster_count;
+
+		float j = cart_go % 50;
+		float t = j / 50.f;
+
+		cart_x = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].x + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].x
+			+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].x + (t*t*t - t * t)*re_coster[p[3]].x) / 2;
+		cart_y = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].y + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].y
+			+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].y + (t*t*t - t * t)*re_coster[p[3]].y) / 2;
+		cart_z = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].z + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].z
+			+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].z + (t*t*t - t * t)*re_coster[p[3]].z) / 2;
+	}
 
 	glutPostRedisplay();
 	glutTimerFunc(20, Timer, 1);
@@ -78,6 +115,7 @@ void Keyboard(unsigned char key, int x, int y)
 		mod = 3;
 		Reshape(1000,1000);
 		glutReshapeFunc(Reshape);
+
 		for (int i = 0; i < coster_count; i++)
 		{
 			re_coster[i].x = coster[i].x - 500;
@@ -85,6 +123,11 @@ void Keyboard(unsigned char key, int x, int y)
 			re_coster[i].z = coster[i].z - 500;
 			re_coster[i].active = coster[i].active;
 		}
+		
+		cart_x = re_coster[0].x;
+		cart_y = re_coster[0].y;
+		cart_z = re_coster[0].z;
+		cart_go = 0;
 		break;
 
 
@@ -102,18 +145,11 @@ void Keyboard(unsigned char key, int x, int y)
 		y_ro -= 5;
 		break;
 
-
-	case 'a'://좌표 테스트 출력
-		for (int i = 0; i < coster_count; i++)
-		{
-			cout << "x : " << coster[i].x << "   y : " << coster[i].y << "   z : " << coster[i].z << endl;
-		}
-		cout << endl;
-		for (int i = 0; i < coster_count; i++)
-		{
-			cout << "x : " << re_coster[i].x << "   y : " << re_coster[i].y << "   z : " << re_coster[i].z << endl;
-		}
-		cout << endl << endl << endl;
+	case 'z':
+		z_zoom += 5;
+		break;
+	case 'Z':
+		z_zoom -= 5;
 		break;
 	}
 }
@@ -217,99 +253,118 @@ void drawScene()
 		glPushMatrix();
 		{
 			gluLookAt(
-				0.0, 500.0, 500.0, //EYE
+				0.0, 200.0, 500.0 - z_zoom, //EYE
 				0.0, 0.0, 0.0, //AT
 				0.0, 1.0, 0.0); //UP
-			
-			glPushMatrix();//2d에서 그리지 않은것
+
+			glRotated(x_ro, 1, 0, 0);
+			glRotated(y_ro, 0, 1, 0);
+
+
+			glPushMatrix();//바닥판
 			{
-				glPushMatrix();//바닥판
-				{
-					glScalef(1000, 1, 1000);
-					glColor3ub(150, 150, 150);
-					glutSolidCube(1);
-				}
-				glPopMatrix();
+				glScalef(1000, 1, 1000);
+				glColor3ub(150, 150, 150);
+				glutSolidCube(1);
 			}
 			glPopMatrix();
 
-			glPushMatrix();//2d에서 그린것들
+			glPushMatrix();//카트
 			{
-				for (int i = 0; i < 10; i++)//장애물
+				glTranslatef(cart_x, cart_y, cart_z);
+
+				glColor3ub(255, 0, 0);
+				glutSolidCube(30);
+			}
+			glPopMatrix();
+
+			for (int i = 0; i < 10; i++)//장애물
+			{
+				glPushMatrix();
 				{
-					glPushMatrix();
+					glTranslatef(barrier[i].x, 0, barrier[i].z);
+
+					glPushMatrix();//나무기둥
 					{
-						glTranslatef(-500, 0, -500);
-						glTranslatef(barrier[i].x, 0, barrier[i].z);
+						glTranslatef(0, 30, 0);
 
 						glColor3ub(155, 90, 61);
-						glBegin(GL_QUADS);
-						glVertex3f(-20, -20, 0);
-						glVertex3f(20, -20, 0);
-						glVertex3f(20, 20, 0);
-						glVertex3f(-20, 20, 0);
-						glEnd();
+						glScalef(1, 3, 1);
+						glutSolidCube(20);
+					}
+					glPopMatrix();
+
+					glPushMatrix();//나뭇잎
+					{
+						glTranslatef(0, 85, 0);
+
+						glColor3ub(0, 208, 85);
+						glutSolidCube(50);
 					}
 					glPopMatrix();
 				}
+				glPopMatrix();
+			}
 
-				for (int i = 0; i < 20; i++)//롤러코스터
+			for (int i = 0; i < 20; i++)//롤러코스터
+			{
+				if (re_coster[i].active == 1)
 				{
-					if (re_coster[i].active == 1)
+					glPushMatrix();//기둥표시
 					{
-						glPushMatrix();//선표시
-						{
-							int p[4];
-
-							p[0] = i - 1;
-							p[1] = i;
-							p[2] = i + 1;
-							p[3] = i + 2;
-
-							if (p[0] == -1)
-								p[0] = coster_count - 1;
-							if (p[2] >= coster_count)
-								p[2] -= coster_count;
-							if (p[3] >= coster_count)
-								p[3] -= coster_count;
-
-							for (int j = 0; j < 49; j++)
-							{
-								float x1, x2, z1, z2, t = j / 50.f;
-
-								x1 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].x + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].x 
-									+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].x + (t*t*t - t * t)*re_coster[p[3]].x) / 2;
-								z1 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].z + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].z 
-									+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].z + (t*t*t - t * t)*re_coster[p[3]].z) / 2;
-
-								t = (j + 1) / 50.f;
-								x2 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].x + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].x 
-									+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].x + (t*t*t - t * t)*re_coster[p[3]].x) / 2;
-								z2 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].z + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].z 
-									+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].z + (t*t*t - t * t)*re_coster[p[3]].z) / 2;
-
-								if (i != coster_count - 1)
-								{
-									glBegin(GL_LINES);
-									glVertex3f(x1, re_coster[i].y - (re_coster[i].y - re_coster[i + 1].y) * j / 50 , z1);
-									glVertex3f(x2, re_coster[i].y - (re_coster[i].y - re_coster[i + 1].y) * (j + 1) / 50, z2);
-									glEnd();
-								}
-								else
-								{
-									glBegin(GL_LINES);
-									glVertex3f(x1, re_coster[i].y - (re_coster[i].y - re_coster[0].y) * j / 50, z1);
-									glVertex3f(x2, re_coster[i].y - (re_coster[i].y - re_coster[0].y) * (j + 1) / 50, z2);
-									glEnd();
-								}
-							}
-						}
-						glPopMatrix();
-
+						glColor3ub(80, 80, 80);
+						glTranslatef(re_coster[i].x, re_coster[i].y / 2.0f, re_coster[i].z);
+						glScalef(1, re_coster[i].y / 10.0f, 1);
+						glutSolidCube(10);
 					}
+					glPopMatrix();
+
+					glPushMatrix();//선표시
+					{
+						int p[4];
+
+						p[0] = i - 1;
+						p[1] = i;
+						p[2] = i + 1;
+						p[3] = i + 2;
+
+						if (p[0] == -1)
+							p[0] = coster_count - 1;
+						if (p[2] >= coster_count)
+							p[2] -= coster_count;
+						if (p[3] >= coster_count)
+							p[3] -= coster_count;
+
+						for (int j = 0; j < 50; j++)
+						{
+							float x1, x2, y1, y2, z1, z2, t = j / 50.f;
+
+							x1 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].x + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].x
+								+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].x + (t*t*t - t * t)*re_coster[p[3]].x) / 2;
+							y1 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].y + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].y
+								+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].y + (t*t*t - t * t)*re_coster[p[3]].y) / 2;
+							z1 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].z + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].z
+								+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].z + (t*t*t - t * t)*re_coster[p[3]].z) / 2;
+
+							t = (j + 1) / 50.f;
+							x2 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].x + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].x
+								+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].x + (t*t*t - t * t)*re_coster[p[3]].x) / 2;
+							y2 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].y + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].y
+								+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].y + (t*t*t - t * t)*re_coster[p[3]].y) / 2;
+							z2 = ((-t * -t * -t + 2 * t * t - t)*re_coster[p[0]].z + (3 * t*t*t - 5 * t*t + 2)*re_coster[p[1]].z
+								+ (-3 * t*t*t + 4 * t*t + t)*re_coster[p[2]].z + (t*t*t - t * t)*re_coster[p[3]].z) / 2;
+
+							glColor3ub(50, 50, 50);
+
+							glBegin(GL_LINES);
+							glVertex3f(x1, y1, z1);
+							glVertex3f(x2, y2, z2);
+							glEnd();
+						}
+					}
+					glPopMatrix();
 				}
 			}
-			glPopMatrix();
 		}
 		glPopMatrix();
 
@@ -361,6 +416,44 @@ void drawScene()
 						glVertex3f(coster[i].x + 10, coster[i].y + 10, 0);
 						glVertex3f(coster[i].x - 10, coster[i].y + 10, 0);
 						glEnd();
+					}
+					glPopMatrix();
+
+					glPushMatrix();//선표시
+					{
+
+						glTranslatef(-500, 0, 0);
+						int p[4];
+
+						p[0] = i - 1;
+						p[1] = i;
+						p[2] = i + 1;
+						p[3] = i + 2;
+
+						if (p[0] == -1)
+							p[0] = coster_count - 1;
+						if (p[2] >= coster_count)
+							p[2] -= coster_count;
+						if (p[3] >= coster_count)
+							p[3] -= coster_count;
+
+						for (int j = 0; j < 49; j++)
+						{
+							float x1, x2, y1, y2, t = j / 50.f;
+
+							x1 = ((-t * -t * -t + 2 * t * t - t)*coster[p[0]].x + (3 * t*t*t - 5 * t*t + 2)*coster[p[1]].x + (-3 * t*t*t + 4 * t*t + t)*coster[p[2]].x + (t*t*t - t * t)*coster[p[3]].x) / 2;
+							y1 = ((-t * -t * -t + 2 * t * t - t)*coster[p[0]].y + (3 * t*t*t - 5 * t*t + 2)*coster[p[1]].y + (-3 * t*t*t + 4 * t*t + t)*coster[p[2]].y + (t*t*t - t * t)*coster[p[3]].y) / 2;
+
+							t = (j + 1) / 50.f;
+							x2 = ((-t * -t * -t + 2 * t * t - t)*coster[p[0]].x + (3 * t*t*t - 5 * t*t + 2)*coster[p[1]].x + (-3 * t*t*t + 4 * t*t + t)*coster[p[2]].x + (t*t*t - t * t)*coster[p[3]].x) / 2;
+							y2 = ((-t * -t * -t + 2 * t * t - t)*coster[p[0]].y + (3 * t*t*t - 5 * t*t + 2)*coster[p[1]].y + (-3 * t*t*t + 4 * t*t + t)*coster[p[2]].y + (t*t*t - t * t)*coster[p[3]].y) / 2;
+
+							glBegin(GL_LINES);
+							glVertex3f(x1, y1, 0);
+							glVertex3f(x2, y2, 0);
+							glEnd();
+						}
+
 					}
 					glPopMatrix();
 				}
